037a70291de4588f7ed8a8da8bb73d8549f6716f
Angelo Melonas <angelomelonas@gmail.com>
2019-08-15 21:03:34 +0200

    The create-cert.sh script generates the certificates/keys for Envoy and the client dev server only.

--- create-cert.sh
+++ create-cert.sh
@@ -15,67 +15,46 @@ CN_NAME=ExampleCoCN
 EMAIL=security@example.com
 PASS=123456
 
-# Clear certificates if they exist
-rm -Rf envoy/certificates
-rm -Rf client/certificates
-rm -Rf server/src/main/resources/keystore
-
-# Create directories for certificates
-echo Attempting to create directories for certificates...
-mkdir envoy/certificates
-mkdir client/certificates
-mkdir server/src/main/resources/keystore
+CERT_OUT_DIR=keystore
+CERT_OUT_ENVOY=envoy/certificates
+CERT_OUT_CLIENT=client/certificates
 
 # Certificate Authority
 echo Generating CA key...
 openssl genrsa -des3 -passout pass:${PASS} \
-        -out envoy/certificates/ca.key 2048
+        -out ${CERT_OUT_DIR}/ca.key 2048
 
 echo Generating CA cetificate...
-openssl req -new -x509 -passin pass:${PASS} -days 365 -key envoy/certificates/ca.key \
-        -out envoy/certificates/ca.crt -subj "//C=${C_CODE}\ST=${STATE}\L=${LOCALITY}\O=${COMPANY}\OU=${UNIT}\CN=${CA_CN_NAME}"
+openssl req -new -x509 -passin pass:${PASS} -days 365 -key ${CERT_OUT_DIR}/ca.key \
+        -out ${CERT_OUT_DIR}/ca.crt -subj "//C=${C_CODE}\ST=${STATE}\L=${LOCALITY}\O=${COMPANY}\OU=${UNIT}\CN=${CA_CN_NAME}"
 
 # Server side key
 echo Generate server key...
 openssl genrsa -des3 -passout pass:${PASS} \
-        -out envoy/certificates/server.key 2048
+        -out ${CERT_OUT_DIR}/server.key 2048
 
 echo Generating server signing request...
-openssl req -new -passin pass:${PASS} -key envoy/certificates/server.key \
-        -out envoy/certificates/server.csr -subj "//C=${C_CODE}\ST=${STATE}\L=${LOCALITY}\O=${COMPANY}\OU=${UNIT}\CN=${CN_NAME}"
+openssl req -new -passin pass:${PASS} -key ${CERT_OUT_DIR}/server.key \
+        -out ${CERT_OUT_DIR}/server.csr -subj "//C=${C_CODE}\ST=${STATE}\L=${LOCALITY}\O=${COMPANY}\OU=${UNIT}\CN=${CN_NAME}"
 
 echo Generating server certificate...
-openssl x509 -req -passin pass:${PASS} -days 365 -in envoy/certificates/server.csr \
-        -CA envoy/certificates/ca.crt -CAkey envoy/certificates/ca.key -set_serial 01 \
-        -out envoy/certificates/server.crt -sha256 -extfile envoy/v3.ext
+openssl x509 -req -passin pass:${PASS} -days 365 -in ${CERT_OUT_DIR}/server.csr \
+        -CA ${CERT_OUT_DIR}/ca.crt -CAkey ${CERT_OUT_DIR}/ca.key -set_serial 01 \
+        -out ${CERT_OUT_DIR}/server.crt -sha256 -extfile ${CERT_OUT_DIR}/config/v3.ext
 
 # Remove servery key passphrase
 echo Removing passphrase from server key...
-openssl rsa -passin pass:${PASS} -in envoy/certificates/server.key \
-        -out envoy/certificates/server.key
+openssl rsa -passin pass:${PASS} -in ${CERT_OUT_DIR}/server.key \
+        -out ${CERT_OUT_DIR}/server.key
 
-echo Creating a keystore for the Spring Boot static file server...
-
-# Create PKCS12 keystore from private key and public certificate
-winpty openssl pkcs12 -export -in envoy/certificates/server.crt -inkey envoy/certificates/server.key \
-               -out envoy/certificates/server.p12 -name myservercert \
-               -passout pass:${PASS} \
-               -CAfile envoy/certificates/ca.crt -caname root
-# Create a proprietary JKS keystore
-keytool -importkeystore \
-        -deststorepass ${PASS} -destkeypass ${PASS} -destkeystore envoy/certificates/server.jks \
-        -srckeystore envoy/certificates/server.p12 -srcstoretype PKCS12 -srcstorepass ${PASS} \
-        -alias myservercert
-
-# Convert from the Java JKS to PKCS12 keystore format
-keytool -importkeystore -srckeystore envoy/certificates/server.jks -srcstorepass ${PASS} -srcalias myservercert \
-        -destkeystore server/src/main/resources/keystore/server.p12 -destalias grpcwebservercert -deststorepass ${PASS} -deststoretype pkcs12 \
-        -noprompt
-
-# Copy certificates to client file server
+# Copy certificates to client file server and Envoy
 echo Copying server key and server certificate to client file server...
-cp -rf envoy/certificates/server.key client/certificates/server.key
-cp -rf envoy/certificates/server.crt client/certificates/server.crt
+cp -rf ${CERT_OUT_DIR}/server.key ${CERT_OUT_CLIENT}/server.key
+cp -rf ${CERT_OUT_DIR}/server.crt ${CERT_OUT_CLIENT}/server.crt
+
+echo Copying server key and server certificate to Envoy...
+cp -rf ${CERT_OUT_DIR}/server.key ${CERT_OUT_ENVOY}/server.key
+cp -rf ${CERT_OUT_DIR}/server.crt ${CERT_OUT_ENVOY}/server.crt
 
 echo Done! Closing in 3 seconds...
 sleep 3
